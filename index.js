d8b5508cb6e59695a70d1c321b5340a9edb66eca
mehfoozmaira413-web <mehfoozmaira413@gmail.com>
2026-08-26 18:19:13 +0500

    fix port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 const app = express()
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 5001
 
 app.use(cors({ 
   origin: "https://threatwatch-client-production.up.railway.app",
